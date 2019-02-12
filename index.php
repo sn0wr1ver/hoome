bd75ce87f8949f6315965df641b258094d16441e
SNOWRIVER <viming.liu@163.com>
2019-02-12 21:34:50 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> 
+<html> 
+<body> 
+
+<?php 
+echo "Hello David!"; 
+?> 
+<div id="info">
+	<a href="/3js/webgl_LIU.html">MY 3JS</a> <br/>
+	<a href="第一个3D场景.html">第一个3D场景</a> <br/>
+
+	<a href="gui库操作界面.html">gui库操作界面</a> <br/>
+	
+</div>
+</body> 
+</html>
